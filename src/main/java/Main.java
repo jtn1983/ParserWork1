9de7717c70fe0bb5e9e4c53cc4a757381bb1f975
Yakov Tenilin <yakov@tenilin.ru>
2021-08-02 17:34:18 +0300

    Add work 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,21 +4,32 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         String jsonFileName = "data.json";
+        String jsonXmlFileName = "data2.json";
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
         writeString(json, jsonFileName);
+        List<Employee> listXml = parseXML("data.xml");
+        String jsonXml = listToJson(listXml);
+        writeString(jsonXml, jsonXmlFileName);
     }
 
     public static List<Employee> parseCSV(String[] columnMapping, String filename) {
@@ -37,15 +48,39 @@ public class Main {
     }
 
     public static String listToJson(List<Employee> list) {
-        Type listType = new TypeToken<List<Employee>>(){}.getType();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
         return new GsonBuilder().setPrettyPrinting().create().toJson(list, listType);
     }
 
-    public static void writeString (String json, String jsonFileName) {
-        try (FileWriter writer = new FileWriter(jsonFileName, false)){
+    public static void writeString(String json, String jsonFileName) {
+        try (FileWriter writer = new FileWriter(jsonFileName, false)) {
             writer.write(json);
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static List<Employee> parseXML(String xmlFileName) throws ParserConfigurationException, IOException, SAXException {
+        List<Employee> employees = new ArrayList<>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(xmlFileName));
+        Node root = doc.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node.getNodeType()) {
+                Element eElement = (Element) node;
+                employees.add(new Employee(
+                        Integer.parseInt(eElement.getElementsByTagName("id").item(0).getTextContent()),
+                        eElement.getElementsByTagName("firstName").item(0).getTextContent(),
+                        eElement.getElementsByTagName("lastName").item(0).getTextContent(),
+                        eElement.getElementsByTagName("country").item(0).getTextContent(),
+                        Integer.parseInt(eElement.getElementsByTagName("age").item(0).getTextContent())
+                ));
+            }
+        }
+        return employees;
     }
 }
